fba1a5b0d196b7fba1d74e3cbb1368ce7588e822
Simon Stender Boisen <simon@skov-boisen.dk>
2015-11-19 00:15:48 +0100

    Prepared mix.exs for hex pm publish

--- mix.exs
+++ mix.exs
@@ -3,27 +3,23 @@ defmodule TeamcityExunitFormatter.Mixfile do
 
   def project do
     [app: :teamcity_exunit_formatter,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
+     description: "A formatter for Elixirs ExUnit that formats as TeamCity Service Messages. Will let you track test results in TeamCitys UI",
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+  defp package do
+    [maintainers: ["Simon Stender Boisen"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/lixhq/teamcity-exunit-formatter"}]
+  end
+
   defp deps do
     []
   end
